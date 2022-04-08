fa06f59812b9921fabf918b88d91ff6e47a48b86
A7-4real <a7.4real@gmail.com>
2022-04-08 23:28:53 +0530

    start react app files

---
+++ react-news-app/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+import React, { Component } from "react";
+import NavBar from "./components/NavBar";
+import News from "./components/News";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <News />
+      </div>
+    );
+  }
+}
