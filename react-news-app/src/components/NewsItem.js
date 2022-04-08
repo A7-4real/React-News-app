a76bc32a1dea4e050c6bce10b2e45fb67aee8c94
A7-4real <a7.4real@gmail.com>
2022-04-08 23:30:20 +0530

    create NewsItem component

---
+++ react-news-app/src/components/NewsItem.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+  render() {
+    let { title, description, imageUrl } = this.props;
+    return (
+      <div>
+        <div className="card" style={{ width: "18rem" }}>
+          <img src={imageUrl} className="card-img-top" alt="" />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href="/" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewsItem;
