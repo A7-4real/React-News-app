dfcac9153f23abfa973beb7d641aee0a766ca4fe
A7-4real <a7.4real@gmail.com>
2022-04-14 21:11:58 +0530

    update NewsItem component

--- react-news-app/src/components/NewsItem.js
+++ react-news-app/src/components/NewsItem.js
@@ -2,16 +2,21 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description, imageUrl } = this.props;
+    let { title, description, imageUrl, srcUrl } = this.props;
     return (
       <div>
-        <div className="card" style={{ width: "18rem" }}>
-          <img src={imageUrl} className="card-img-top" alt="" />
+        <div className="card" style={{ width: "18rem", height: "30rem" }}>
+          <img
+            src={imageUrl}
+            className="card-img-top"
+            alt=""
+            style={{ height: "200px", width: "285px" }}
+          />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="/" className="btn btn-primary">
-              Go somewhere
+            <a href={srcUrl} target="_blank" className="btn btn-dark">
+              Read more
             </a>
           </div>
         </div>
