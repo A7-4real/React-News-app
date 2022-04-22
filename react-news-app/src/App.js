48d336d7d6181dd229a1815926504b9efb8dc9a3
A7-4real <a7.4real@gmail.com>
2022-04-22 23:03:58 +0530

    update category props to general

--- react-news-app/src/App.js
+++ react-news-app/src/App.js
@@ -9,7 +9,7 @@ export default class App extends Component {
     return (
       <div>
         <NavBar />
-        <News pageSize={9} country="in" category="technology" />
+        <News pageSize={9} country="in" category="general" />
       </div>
     );
   }
