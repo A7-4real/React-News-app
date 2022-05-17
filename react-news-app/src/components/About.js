68a96347afa7f4845eade50123fc5fcee1e48ced
A7-4real <a7.4real@gmail.com>
2022-05-17 22:44:43 +0530

    About Component

---
+++ react-news-app/src/components/About.js
@@ -0,0 +1,12 @@
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+
+export class About extends Component {
+  static propTypes = {};
+
+  render() {
+    return <div> About us</div>;
+  }
+}
+
+export default About;
